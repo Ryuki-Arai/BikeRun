9251b22432b04158d6b76eb4e4d923bd4cc63769
Ryu-0824 <83778052+Ryuki-Arai@users.noreply.github.com>
2023-02-25 00:35:00 +0900

    Player：任意で指定可能なn段ジャンプ機能を追加

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField, Tooltip("ジャンプ力")] int _jumpPower;
+    [SerializeField,Tooltip("連続ジャンプできる回数")] int _jumpMaxCount;
+    int _count;
+    Rigidbody _rb;
+
+
+    void OnEnable()
+    {
+        _count = _jumpMaxCount;
+        _rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        if (Input.GetButtonDown("Jump")/*Input.GetMouseButtonDown(0)*/)
+        {
+            if(_count > 0)
+            {
+                _count--;
+                _rb.velocity = Vector3.zero;
+                _rb.AddForce(_jumpPower * transform.up, ForceMode.Impulse);
+                Debug.Log($"jump：残り{_count}回");
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        _count = _jumpMaxCount;
+        Debug.Log($"リセット：残り{_count}回");
+    }
+}
