5c16a67e021904b0241ab3e3d1df6410c30f24c4
Ryu-0824 <83778052+Ryuki-Arai@users.noreply.github.com>
2023-03-04 01:00:13 +0900

    パーリンノイズの導入実験

--- Assets/Scripts/GeneratePerlinTexture.cs
+++ Assets/Scripts/GeneratePerlinTexture.cs
@@ -1,74 +1,97 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 /// <Summary>
-/// パーリンノイズのテクスチャを生成するクラスです。
+/// パーリンノイズを使ってフィールドを生成するクラスです。
 /// </Summary>
 public class GeneratePerlinTexture : MonoBehaviour
 {
-    // テクスチャを表示するイメージへの参照です。
-    public RawImage rawImage;
+    // フィールド作成のためのパーツへの参照です。
+    public GameObject fieldParts;
 
-    // 生成したテクスチャです。
-    Texture2D texture;
+    // フィールドパーツの親オブジェクトのTransformです。
+    public Transform fieldParent;
 
+    // フィールドに関する情報です。
+    public int fieldSizeX = 50;
+    public int fieldSizeZ = 50;
+    public int fieldHeight = 50;
+
+    // パーリンノイズに関する情報です。
     public float xOrigin;
     public float yOrigin;
     public float scale = 0.01f;
 
-    /// <Summary>
-    /// パーリンノイズによる数列を生成します。
-    /// </Summary>
-    /// <param name="length"></param>
-    void GeneratePerlinArray(int length)
+    void Start()
     {
-        List<float> perlinArrayList = new List<float>();
-        float y = 0.5f;
-        for (int i = 0; i < length; i++)
-        {
-            // 0から1までのパーリンノイズの値を取得します。
-            float xValue = xOrigin + i * scale;
-            float yValue = yOrigin + y * scale;
-            float value = Mathf.PerlinNoise(xValue, yValue);
-            perlinArrayList.Add(value);
-        }
 
-        GenerateTexture(perlinArrayList);
+    }
+
+    void Update()
+    {
+
     }
 
     /// <Summary>
-    /// 引数のリストを元にテクスチャを生成します。
+    /// 生成ボタンが押された時のメソッドです。
     /// </Summary>
-    /// <param name="length"></param>
-    void GenerateTexture(List<float> valueList)
+    public void OnPressedGenerateButton()
     {
-        int texWidth = valueList.Count;
-        int texHeight = (int)rawImage.rectTransform.sizeDelta.y;
-        texture = new Texture2D(texWidth, texHeight);
-        Color[] colorArray = new Color[texWidth * texHeight];
-
-        for (int i = 0; i < texWidth * texHeight; i++)
-        {
-            colorArray[i] = Color.white;
-        }
-
-        for (int i = 0; i < valueList.Count; i++)
-        {
-            int y = Mathf.RoundToInt(valueList[i] * 100);
-            Color col = Color.black;
-            colorArray[i + texWidth * y] = col;
-        }
-
-        texture.SetPixels(colorArray);
-        texture.Apply();
-        rawImage.texture = texture;
+        GenerateFieldParts();
     }
 
-    public void OnPressedPerlinButton()
+    /// <Summary>
+    /// フィールドを生成するメソッドです。
+    /// </Summary>
+    void GenerateFieldParts()
     {
-        int width = (int)rawImage.rectTransform.sizeDelta.x;
-        GeneratePerlinArray(width);
+        for (float z = 0f; z < fieldSizeZ; z++)
+        {
+            for (float x = 0f; x < fieldSizeX; x++)
+            {
+                // パーリンノイズの座標を指定して値を取得します。
+                float xValue = xOrigin + x * scale;
+                float yValue = yOrigin + z * scale;
+                float perlinValue = Mathf.PerlinNoise(xValue, yValue);
+                float height = fieldHeight * perlinValue;
+
+                // 位置のVector3を作成してオブジェクトをインスタンス化します。
+                Vector3 pos = new Vector3(x, height, z);
+                InstantiateFieldParts(pos);
+            }
+        }
+    }
+
+    /// <Summary>
+    /// オブジェクトをインスタンス化するメソッドです。
+    /// </Summary>
+    void InstantiateFieldParts(Vector3 pos)
+    {
+        // オブジェクトをインスタンス化します。
+        GameObject obj = Instantiate(fieldParts, Vector3.zero, Quaternion.identity);
+
+        // オブジェクトのTransformを設定します。
+        obj.transform.SetParent(fieldParent);
+        obj.transform.localPosition = pos;
+    }
+
+    /// <Summary>
+    /// 削除ボタンが押された時のメソッドです。
+    /// </Summary>
+    public void OnPressedRemoveButton()
+    {
+        RemoveFieldParts();
+    }
+
+    /// <Summary>
+    /// フィールドのパーツを削除するメソッドです。
+    /// </Summary>
+    void RemoveFieldParts()
+    {
+        foreach (Transform tran in fieldParent)
+        {
+            Destroy(tran.gameObject);
+        }
     }
 }
