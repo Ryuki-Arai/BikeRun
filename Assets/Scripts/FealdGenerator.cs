157c37f1cae31ca4a510408971638dd4c9c8a5e1
Ryu-0824 <83778052+Ryuki-Arai@users.noreply.github.com>
2023-03-04 00:58:14 +0900

    フィールドの生成に高低差を付ける(完成)

--- Assets/Scripts/FealdGenerator.cs
+++ Assets/Scripts/FealdGenerator.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class FealdGenerator : MonoBehaviour
 {
-    [SerializeField] GameObject _ground;
-    [SerializeField] Vector3 _currentPos;
-    [SerializeField] int _instabilityY;
-    [SerializeField] int _generatInterval;
-    [SerializeField] int _holeProbability;
+    // フィールド作成のためのパーツへの参照です。
+    [SerializeField] GameObject fieldParts;
+
+    // フィールドパーツの親オブジェクトのTransformです。
+    [SerializeField] Transform fieldParent;
+
+    [Header("フィールドに関する情報")]
+    const int FIELD_X = 1;
+    [SerializeField] int fieldSizeZ = 50;
+    [SerializeField] int fieldHeight = 20;
+
+    [Header("パーリンノイズに関する情報")]
+    [SerializeField] float xOrigin;
+    [SerializeField] float yOrigin;
+    [SerializeField] float scale = 0.05f;
 
     void Start()
     {
-        StartCoroutine(FealdGenerate());
+        StartCoroutine(GenerateFieldParts());
     }
 
     void Update()
@@ -20,12 +30,54 @@ public class FealdGenerator : MonoBehaviour
         
     }
 
-    IEnumerator FealdGenerate()
+    /// <Summary>
+    /// フィールドを生成するメソッドです。
+    /// </Summary>
+    //void GenerateFieldParts()
+    //{
+    //    for (float z = 0f; z < fieldSizeZ; z++)
+    //    {
+    //        // パーリンノイズの座標を指定して値を取得します。
+    //        float xValue = xOrigin + FIELD_X * scale;
+    //        float yValue = yOrigin + z * scale;
+    //        float perlinValue = Mathf.PerlinNoise(xValue, yValue);
+    //        float height = fieldHeight * perlinValue;
+
+    //        // 位置のVector3を作成してオブジェクトをインスタンス化します。
+    //        Vector3 pos = new Vector3(FIELD_X, height, z);
+    //        InstantiateFieldParts(pos);
+    //    }
+    //}
+
+    IEnumerator GenerateFieldParts()
     {
+        int z = 0;
         while (true)
         {
-            Instantiate(_ground,new Vector3(_currentPos.x,_currentPos.y + Random.Range(-_instabilityY,_instabilityY),_currentPos.z),Quaternion.identity);
-            yield return new WaitForSeconds(_generatInterval);
+            // パーリンノイズの座標を指定して値を取得します。
+            float xValue = xOrigin + FIELD_X * scale;
+            float yValue = yOrigin + z * scale;
+            float perlinValue = Mathf.PerlinNoise(xValue, yValue);
+            float height = fieldHeight * perlinValue;
+
+            // 位置のVector3を作成してオブジェクトをインスタンス化します。
+            Vector3 pos = new Vector3(FIELD_X, height, z);
+            InstantiateFieldParts(pos);
+            z++;
+            yield return null;
         }
+    }
+
+    /// <Summary>
+    /// オブジェクトをインスタンス化するメソッドです。
+    /// </Summary>
+    void InstantiateFieldParts(Vector3 pos)
+    {
+        // オブジェクトをインスタンス化します。
+        GameObject obj = Instantiate(fieldParts, Vector3.zero, Quaternion.identity);
+
+        // オブジェクトのTransformを設定します。
+        obj.transform.SetParent(fieldParent);
+        obj.transform.localPosition = pos;
     }
 }
